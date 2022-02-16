cccb04a171e08b8069dd949ea245dd4f87a00b69
hasanosman2020 <hasan79.hoa@gmail.com>
2022-02-16 19:52:14 +0000

    initial commit

---
+++ lesson 8/assignment_Rock_Paper_Scissors/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson 8 - Rock, 
+        Paper, Scissors
+    </title>
+    <?php require_once 'bootstrap.php'; ?>
+</head>
+<body>
+    <div class='container'>
+        <h1>Welcome to Rock Paper Scissors</h1>
+        <p><a href='login.php'>Please log in</a></p>
+        <p>Attempt to go to <a href='game.php'>game.php</a> without logging in -  it should fail with an error message. </p>
+        <p><a href='#'>source code</a></p>
+    </div>
+    
+</body>
+</html>
