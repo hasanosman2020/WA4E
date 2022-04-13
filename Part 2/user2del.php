b20f0f8e65d2699285edf287495f73599217f7fd
hasanosman2020 <hasan79.hoa@gmail.com>
2022-04-13 23:24:04 +0100

    DOCS: lesson 12 part 2- - insert data, delete data, create delete buttons. BUG: delete button does not work.

---
+++ Part 2/user2del.php
@@ -0,0 +1,21 @@
+
+<?php
+require_once "pdo.php";
+
+if(isset($_POST['user_id'])){
+$sql ='DELETE FROM users WHERE user_id = :zip';
+echo "<pre>\n$sql\n</pre>\n";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(array(':zip' => $_POST['user_id']));
+}
+
+
+?>
+
+<p>Delete a User</p>
+<form method='post'>
+<p>ID to Delete:
+<input type='text' name='user_id'</p>
+<p><input type='submit' value='Delete' /></p>
+</form>
+
