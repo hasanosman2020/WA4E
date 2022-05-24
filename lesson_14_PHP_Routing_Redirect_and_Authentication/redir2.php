9342e6fbea3ebe4f2c11fba47aa16a6c32efc1b3
hasanosman2020 <hasan79.hoa@gmail.com>
2022-05-24 20:37:20 +0100

    DOCS: complete part 1 of lesson 14 dealing with code 302 and http Redirect

---
+++ lesson_14_PHP_Routing_Redirect_and_Authentication/redir2.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if(isset($_POST['where'])){
+    if($_POST['where']=='1'){
+        header('Location:redir1.php');
+        return;
+    }else if($_POST['where'] == '2'){
+        header('Location:redir2.php');
+        return;
+        }else{
+            header('Location:http://www.dr-chuck.com');
+            return;
+        }
+    }
+?>
+<html>
+    <body style='font-family: sans-serif;'>
+<p>I am Router Two...</p>
+<form method='post'>
+    <p><label for='inp9'>Where to go?</label>
+<input type='text' name='where' id='inp9' size='5'></p>
+<input type='submit'>
+</form>
+</body>
+</html>
