69a78301f85cf6d886416b80d3ecf3724f6cdd1d
hasanosman2020 <hasan79.hoa@gmail.com>
2022-06-16 12:05:06 +0100

    add guess.php

---
+++ lesson_14_PHP_Routing_Redirect_and_Authentication/14.2 PHP_POST_Redirect/guess.php
@@ -0,0 +1,38 @@
+<?php
+$guess = '';
+$message = false;
+if (isset($_POST['guess'])){
+    //trick for integer/numeric parameters
+    $guess = $_POST['guess'] + 0;
+    if ($guess = 42){
+        $message = 'Great job!';
+    } else if ($guess < 42){
+        $message = 'Too low!';
+    } else {
+        $message = 'Too high!';
+    }
+}
+?>
+<html>
+    <head>
+        <title>A Guessing Game</title>
+    </head>
+    <body style='font-family: sans-serif;'>
+<p>Guessing Game...</p>
+<?php
+if ($message !== false){
+    echo("<p>$message</p>\n");
+}
+?>
+<form method='post'>
+    <p><label for='guess'>Input Guess</label>
+<input type='text' name='guess' id='guess' size='50'
+<?php
+echo 'value="'.htmlentities($guess). '"';
+?>
+/>
+</p>
+<input type='submit'>
+</form>
+</body>
+</html>
